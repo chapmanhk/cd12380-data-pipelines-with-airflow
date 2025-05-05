4acb0d6228050c4339d58588a042d505338aa23f
chapmanhk <hannahxchapman@yahoo.com>
2025-05-05 12:58:51 -0700

    initialize class with aws and redshift arguments

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -7,15 +7,21 @@ class StageToRedshiftOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 json_path="auto",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json_path = json_path
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
