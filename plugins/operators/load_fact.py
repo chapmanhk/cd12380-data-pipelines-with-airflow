807e16d05d9e01d870b2cccff79d9e295e8b1b6c
chapmanhk <hannahxchapman@yahoo.com>
2025-05-09 20:42:25 -0700

    Implemented load fact operator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -8,15 +8,26 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_query="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query = sql_query
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info(f"Connecting to Redshift: {self.redshift_conn_id}")
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info(f"Loading data into fact table {self.table}")
+        insert_sql = f"""
+            INSERT INTO {self.table}
+            {self.sql_query}
+        """
+        self.log.info(f"Executing SQL:\n{insert_sql}")
+        redshift.run(insert_sql)
+
+        self.log.info(f"Successfully loaded data into {self.table}")
